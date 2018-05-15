69c17c257328a9fedf1583c5e2acb43ead2aebce
RedLeader12 <redleader@jigsaw.xyz>
2018-05-15 10:37:14 +0800

    Learnt how to pass arguments into methods which are being passed into props

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log('clicked!')
     this.setState({  
        persons: [
-      {name: 'Maximmilian', age: '30'},
+      {name: newName, age: '30'},
       {name: 'Dania', age: '24'},
       {name: 'Hey', age: '15'},
     ]})
@@ -26,9 +26,9 @@ class App extends Component {
     return (
       <div className="App">
       <p> Hello </p> 
-      <button onClick={this.switchNameHandler}> Switch Name </button> 
+      <button onClick={() => this.switchNameHandler('Maxine!')}> Switch Name </button> 
         < Person name={this.state.persons[0].name} age={this.state.persons[0].age}/> 
-        < Person name={this.state.persons[1].name} age={this.state.persons[1].age}> Hobbies: Cats </Person>  
+        < Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'Max')}> Hobbies: Cats </Person>  
         < Person name={this.state.persons[2].name} age={this.state.persons[2].age}/> 
       </div>
     );
