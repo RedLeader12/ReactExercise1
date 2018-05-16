aff383e57b4f8b7d414cb0bb0cf675cfb4429c21
RedLeader12 <redleader@jigsaw.xyz>
2018-05-17 07:38:58 +0800

    Changed Persons, Person to a class than a component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import Person from './Person/Person'
+import React, { Component }from 'react';
+import Person from './Person/Person';
 
-const persons = (props) =>  props.persons.map((person,index) => {
- return <Person
-         click={() => props.clicked(index)}
-         change={(event) => props.changed(event, person.id)}
-         name={person.name}
-         age={person.age}
-         key={person.id}
-         /> 
-      });
+class Persons extends Component {
 
-export default persons; 
+render(){
+      return this.props.persons.map((person,index) => {
+            return <Person
+                    click={() => this.props.clicked(index)}
+                    change={(event) => this.props.changed(event, person.id)}
+                    name={person.name}
+                    age={person.age}
+                    key={person.id}
+                    /> 
+                 });
+            } 
+}
+
+export default Persons; 
