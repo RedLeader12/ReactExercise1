640683d225e18dec72db8fff05ee761159d8b01b
RedLeader12 <redleader@jigsaw.xyz>
2018-05-17 08:24:18 +0800

    Console logged the lifecycles to observe what order it goes in between components and between lifecycle types

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,18 +2,48 @@ import React, { Component }from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
+constructor(props){
+    super(props)
+    console.log('[Persons.js]')
+  }
+
+  componentWillMount(){
+      console.log('[Persons.js] inside CWM')
+    }
+  
+    componentDidMount(){
+      console.log('[Persons.js] inside CWD')
+    }
+
+    componentWillReceiveProps( nextProps){
+          console.log('[UPDATE] Persons.js inside CWRP ')
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+          console.log('[Update] Persons.js sCU', nextProps, nextState)
+          return nextProps.persons !== this.props.persons;
+    }
+
+    componentWillUpdate(nextProps, nextState){
+          console.log('[Update] Persons.js cWU', nextProps, nextState)
+    }
+
+    componentDidUpdate(nextProps, nextState){
+          console.log('[Update] Persons.js CDM', nextProps, nextState)
+    }
 
 render(){
-      return this.props.persons.map((person,index) => {
-            return <Person
-                    click={() => this.props.clicked(index)}
-                    change={(event) => this.props.changed(event, person.id)}
-                    name={person.name}
-                    age={person.age}
-                    key={person.id}
+ console.log('[App.js] inside renders')
+    return this.props.persons.map((person,index) => {
+       return <Person
+       click={() => this.props.clicked(index)}
+       change={(event) => this.props.changed(event, person.id)}
+       name={person.name}
+       age={person.age}
+       key={person.id}          
                     /> 
-                 });
-            } 
+            });
+      } 
 }
 
 export default Persons; 
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,14 +4,25 @@ import Cockpit from '../components/Cockpit/Cockpit';
 import Persons from '../components/Persons/Persons';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    console.log('[App.js]')
+    this.state = {
+      persons: [
+        {id: 'asdfa', name: 'Max', age: '30'},
+        {id: '1232', name: 'Dania', age: '24'},
+        {id: '1245', name: 'Hey', age: '15'},
+      ],
+      showPersons: false
+    }
+  }
 
-  state = {
-    persons: [
-      {id: 'asdfa', name: 'Max', age: '30'},
-      {id: '1232', name: 'Dania', age: '24'},
-      {id: '1245', name: 'Hey', age: '15'},
-    ],
-    showPersons: false
+  componentWillMount(){
+    console.log('[App.js] inside CWM')
+  }
+
+  componentDidMount(){
+    console.log('[App.js] inside CWD')
   }
 
   togglePersonsHandler = () => {
@@ -40,7 +51,7 @@ class App extends Component {
   }
 
   render() {
-
+    console.log('[App.js] inside renders')
     let persons = null 
 
 
